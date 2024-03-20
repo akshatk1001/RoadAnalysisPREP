f659e1652291e91b0d4e54e9fed6b219c737754a
Akshat Khandelwal <akshatk1001@gmail.com>
2024-03-19 23:23:22 -0700

    worked on using csv

--- PandasDataExploration.py
+++ PandasDataExploration.py
@@ -1,21 +1,32 @@
 import pandas as pd
 import numpy as np
 
-a1 = np.random.randint(low = 1, high = 50, size = (10, 4))
-titles = ["a", "b", "c", "d"]
+# a1 = np.random.randint(low = 1, high = 50, size = (10, 4))
+# titles = ["a", "b", "c", "d"]
 
-df1 = pd.DataFrame(data = a1, columns = titles)
+# df1 = pd.DataFrame(data = a1, columns = titles)
 
-print("\n", df1, "\n")
+# print("\n", df1, "\n")
 
-print(df1.head(2), "\n")
+# print(df1.head(2), "\n")
 
-print(df1.iloc[2], "\n")
+# print(df1.iloc[2], "\n")
 
-df1["e"] = df1["a"] + df1["b"]
+# df1["e"] = df1["a"] + df1["b"]
 
 
-new_row = pd.DataFrame(data = [np.random.randint(low = 1, high = 50, size = len(df1.columns))], columns = df1.columns)
-df1 = pd.concat([df1, new_row], ignore_index = True)
+# new_row = pd.DataFrame(data = [np.random.randint(low = 1, high = 50, size = len(df1.columns))], columns = df1.columns)
+# df1 = pd.concat([df1, new_row], ignore_index = True)
 
-print(df1, "\n")
+# print(df1, "\n")
+
+
+a2 = pd.read_csv("data.csv")
+
+# print(a2, "\n")
+
+print(a2.head(2), "\n")
+
+a2["Cals 2"] = a2["Calories"] * 2
+
+print(a2["Cals 2"], "\n")
