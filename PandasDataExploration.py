355421202f668a8294fe7ea5c4238b3ee6c0f685
Akshat Khandelwal <akshatk1001@gmail.com>
2024-03-20 00:11:31 -0700

    Found average of all columns

--- PandasDataExploration.py
+++ PandasDataExploration.py
@@ -23,10 +23,18 @@ import numpy as np
 
 a2 = pd.read_csv("data.csv")
 
-# print(a2, "\n")
+print(a2, "\n")
 
 print(a2.head(2), "\n")
 
 a2["Cals 2"] = a2["Calories"] * 2
 
-print(a2["Cals 2"], "\n")
+del a2["Date"]
+
+
+nr = pd.DataFrame(data = [np.array([a2["Duration"].mean(), a2["Pulse"].mean(), a2["Maxpulse"].mean(), a2["Calories"].mean(), a2["Cals 2"].mean()])], columns = a2.columns)
+
+
+a2 = pd.concat([a2, nr], ignore_index = True)
+
+print(a2, "\n")
